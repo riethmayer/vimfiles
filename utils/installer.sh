22518a9bbc8a1e1c2058331eba1b1f9b81031b0f
Artur Roszczyk <artur.roszczyk@gmail.com>
2013-03-26 00:04:19 +0100

    Update installer.sh
    Use ssh version (less troubles ;))

--- utils/installer.sh
+++ utils/installer.sh
@@ -4,7 +4,7 @@ if [ -d $HOME/.vim ]
 then
     cd ~/.vim ; git pull
 else
-    cd ~ ; git clone https://github.com/andrzejsliwa/vimfiles.git ~/.vim; ln -s $HOME/.vim/vimrc $HOME/.vimrc ; cd -
+    cd ~ ; git clone git@github.com:andrzejsliwa/vimfiles.git ~/.vim; ln -s $HOME/.vim/vimrc $HOME/.vimrc ; cd -
 fi
 ln -s $HOME/.vim/vimrc $HOME/.vimrc
 ln -s $HOME/.vim/tmux.conf $HOME/.tmux.conf
